5a70f533929322e08ba8e2447215aeb4350c28af
haotrn <hao.tran@rwth-aachen.de>
2025-01-11 17:11:01 +0100

    add support for github cli

--- java-research-template/hooks/post_gen_project.py
+++ java-research-template/hooks/post_gen_project.py
@@ -15,28 +15,47 @@ def remove_unnecessary_files():
             os.unlink(path) if os.path.isfile(path) else os.rmdir(path)
 
 
-def check_build_tool_installed():
-    build_tool = "{{ cookiecutter.build_tool }}"
+def tool_installed_check(tool, log_level) -> bool:
     try:
-        if build_tool == "Gradle":
-            subprocess.run(["gradle", "--version"],
-                           capture_output=True, check=True)
-        if build_tool == "Maven":
-            subprocess.run(["mvn", "--version"],
-                           capture_output=True, check=True)
+        subprocess.run([tool, "--version"],
+                       capture_output=True, check=True)
+        return True
     except Exception:
-        print(f"Warning: {build_tool} not installed.")
+        print(f"{log_level}: {tool} not installed.")
+        return False
+
+
+def check_build_tool_installed():
+    if "{{ cookiecutter.build_tool }}" == "Gradle":
+        tool = "gradle"
+    elif "{{ cookiecutter.build_tool }}" == "Maven":
+        tool = "mvn"
+    tool_installed_check(tool, "Warning")
 
 
 def init_git_repository():
     # working with strings in order to maintain valid python syntax
-    if "{{ cookiecutter.init_git_repository }}" == "True":
+    if "{{ cookiecutter.init_git_repository }}" == "True" \
+            and tool_installed_check("git", "Error"):
         subprocess.run(["git", "init"], capture_output=True, check=True)
         subprocess.run(["git", "add", "."], capture_output=True, check=True)
         subprocess.run(["git", "commit", "-m", "Initial commit"],
                        capture_output=True, check=True)
 
 
+def create_remote_repository():
+    if "{{ cookiecutter.git_hosting_service }}" == "GitHub" \
+            and "{{ cookiecutter.init_git_repository }}" == "True" \
+            and tool_installed_check("gh", "Error"):
+        subprocess.run(["gh", "repo", "create",
+                        "{{ cookiecutter.__project_slug }}", "--public",
+                        "--source=.", "--remote=upstream"],
+                       capture_output=True, check=True)
+        subprocess.run(["git", "push", "--set-upstream", "upstream",
+                        "main"], capture_output=True, check=True)
+
+
 check_build_tool_installed()
 remove_unnecessary_files()
 init_git_repository()
+create_remote_repository()
